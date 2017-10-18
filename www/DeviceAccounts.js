09919ec46b623b7d2bec830f2d648a18be9efee9
kjepper <kjepper@users.noreply.github.com>
2017-10-18 11:17:10 +0200

    Update DeviceAccounts.js

--- www/DeviceAccounts.js
+++ www/DeviceAccounts.js
@@ -1,28 +1,36 @@
-function DeviceAccounts(){};
+var deviceAccounts = {
+    get: function (onSuccess, onFail) {
+        cordova.exec(onSuccess, onFail, 'DeviceAccounts', 'getDeviceAccounts', []);
+    },
 
-DeviceAccounts.get = function(onSuccess, onFail){
-  cordova.exec(onSuccess, onFail, 'DeviceAccounts', 'getDeviceAccounts', []);
-};
-DeviceAccounts.getByType = function(type, onSuccess, onFail){
-  cordova.exec(onSuccess, onFail, 'DeviceAccounts', 'getDeviceAccountsByType', [type]);
-};
-DeviceAccounts.getEmails = function(onSuccess, onFail){
-  DeviceAccounts.getByType('com.google', function(accounts){
-    var emails = [];
-    for(var i in accounts){
-      emails.push(accounts[i].name);
-    }
-    onSuccess(emails);
-  }, onFail);
-};
-DeviceAccounts.getEmail = function(onSuccess, onFail){
-  DeviceAccounts.getEmails(function(emails){
-    if(emails && emails.length > 0){
-      onSuccess(emails[0]);
-    } else {
-      onSuccess();
-    }
-  }, onFail);
-};
+    getByType: function (type, onSuccess, onFail) {
+        cordova.exec(onSuccess, onFail, 'DeviceAccounts', 'getDeviceAccountsByType', [type]);
+    },
 
-module.exports = DeviceAccounts;
+    getEmails: function (onSuccess, onFail) {
+        DeviceAccounts.getByType('com.google', function (accounts) {
+            var emails = [];
+            for (var i in accounts) {
+                emails.push(accounts[i].name);
+            }
+            onSuccess(emails);
+        }, onFail);
+    },
+
+    getEmail: function (onSuccess, onFail) {
+        DeviceAccounts.getEmails(function (emails) {
+            if (emails && emails.length > 0) {
+                onSuccess(emails[0]);
+            } else {
+                onSuccess();
+            }
+        }, onFail);
+    }
+}
+
+if (!window.plugins) {
+    window.plugins = {};
+}
+
+window.plugins.deviceAccounts = deviceAccounts;
+return window.plugins.deviceAccounts;
